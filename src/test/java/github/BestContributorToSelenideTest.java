49dc9cd61c54a53b3b4020c28621ab43a9564946
Илья <50883558+Iliapopov91@users.noreply.github.com>
2026-03-06 23:32:13 +0300

    update FirstContributer

--- src/test/java/github/BestContributorToSelenideTest.java
+++ src/test/java/github/BestContributorToSelenideTest.java
@@ -1,9 +1,12 @@
 package github;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class BestContributorToSelenideTest {
@@ -31,6 +34,11 @@ public class BestContributorToSelenideTest {
         open("/selenide/selenide");
 
         //Навести курсор на первого контрибьютера
+        $("div.prc-PageLayout-Pane-AyzHK").$(Selectors.byText("Contributors"))
+                .closest(".BorderGrid-cell").$$("ul li").first().hover();
+
+        Selenide.sleep(5000);
         //Проверка окна на содержание текста Andrey Solntsev
+
     }
 }
