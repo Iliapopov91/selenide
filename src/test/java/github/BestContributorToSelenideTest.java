5958981ff3fb196b465f75197cdc139a2fba0a4c
Илья <50883558+Iliapopov91@users.noreply.github.com>
2026-03-06 17:06:42 +0300

    BestContributorToSelenideTest

---
+++ src/test/java/github/BestContributorToSelenideTest.java
@@ -0,0 +1,33 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class BestContributorToSelenideTest {
+
+    @BeforeAll
+    static void beforeAll() {
+
+        //Задаём разрешение браузера
+        Configuration.browserSize= "2560x1440";
+
+        //Задаём URL
+        Configuration.baseUrl = "https://github.com/";
+
+        //Пропускаем полную загрузку страницы
+        Configuration.pageLoadStrategy = "eager";
+
+        //Оставляем браузер открытым
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    public void selenideBestContributor() {
+
+        //Открыть Selenide
+        open("/selenide/selenide");
+    }
+}
